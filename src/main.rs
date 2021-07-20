a4bfb27cc0bc9eec778a72a504a3b650cc8bae43
ra <davidchen0@protonmail.com>
2021-07-20 16:50:13 -0400

    load ringtone from home directory

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::time::{Duration, Instant};
+use std::time::Duration;
 use std::thread::sleep;
 use text_io::read;
 use std::fs::File;
@@ -6,26 +6,45 @@ use std::io::{BufReader, self, Write};
 use rodio::{Decoder, OutputStream, source::Source};
 use std::*;
 use std::convert::TryInto;
+use std::io::prelude::*;
+use home::home_dir;
 
 fn main() {
+    let mut stdin = io::stdin();
+    let mut stdout = io::stdout();
     
     print!("Length of timer(in seconds): ");
     io::stdout().flush().unwrap();
     
     let time_to_wait: i32 = read!();
 
+    // sleep for the timer duration
     sleep(Duration::new(time_to_wait.try_into().unwrap(), 0));
+
 
     // Get a output stream handle to the default physical sound device
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-    // Load a sound from a file, using a path relative to Cargo.toml
-    let file = BufReader::new(File::open("ringtone.mp3").unwrap());
+
+    // Load a sound from a file, using a path relative to home
+    let ringtone_file;
+    match home::home_dir() {
+        Some(path) => ringtone_file = BufReader::new(File::open(path.display().to_string() + "/ringtone.mp3").unwrap()),
+        None => { 
+            println!("Impossible to get your home dir!");
+            process::exit(0);
+        },
+    };
+
     // Decode that sound file into a source
-    let source = Decoder::new(file).unwrap();
+    let source = Decoder::new(ringtone_file).unwrap();
     // Play the sound directly on the device
     stream_handle.play_raw(source.convert_samples());
 
     // The sound plays in a separate audio thread,
     // so we need to keep the main thread alive while it's playing.
-    sleep(Duration::from_secs(5));
+    print!("Timer of {} seconds completed! Press any key to exit:", time_to_wait);
+    io::stdout().flush().unwrap();
+
+    // Read a single byte and discard
+    let _ = stdin.read(&mut [0u8]).unwrap();
 }
